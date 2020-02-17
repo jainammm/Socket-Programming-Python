0e38bbc5965246e60278008a8dac4c203ef9717e
jainammm <jainam.iitrpr@gmail.com>
2020-02-17 17:41:18 +0530

    Check username and password of only one client socket

--- client.py
+++ client.py
@@ -1,7 +1,40 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
+def main():
+    hostname = input("Enter Hostname: ")
+    port = int(input("Enter Port number: "))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        print(clientSocket.connect((hostname, port)))
+    except ConnectionRefusedError:
+        print("Please specify correct hostname and port number!")
+        return
+
+    # Message of successfull connection!
+    print(f"Connection with {hostname} has been established.")
+
+    # Enters username and sends to the server
+    print("=========================================")
+    username = input("Please enter the username: ")
+    clientSocket.send(username.encode())
+
+    # Print ack received by the server
+    ack = clientSocket.recv(1024).decode()
+    print("=========================================")
+    print(f"{ack}")
+
+    # Enter the password and send to the server
+    print("=========================================")
+    password = input("Please enter the password: ")
+    clientSocket.send(password.encode())
+
+    ack = clientSocket.recv(1024).decode()
+    print("=========================================")
+    print(f"{ack}")
+    
+    # Close connection with server
+    clientSocket.close()
+
+if __name__== "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,11 +1,58 @@
 import socket
+import csv
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+usernamePasswordDict = {}
 
-while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!!","utf-8"))
+with open('login_credentials.csv', 'r') as file_data:
+    reader = csv.DictReader(file_data, ("Username", "Password"))
+    for i in reader:
+        usernamePasswordDict[i["Username"]] = i["Password"]
+
+def checkPassword(username, password):
+    try:
+        uPass = usernamePasswordDict[username]
+    except:
+        uPass = "NA"
+
+    if uPass == password: 
+        return "ACK: User has logged in!"
+    
+    return "ACK: Please enter correct password or username!"
+
+def main():
+    hostname = socket.gethostname()
+    port = int(input("Enter Port number: "))
+
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((hostname, port))
+    serverSocket.listen(5)
+
+    print(f"The server has started running with host {hostname} and port {port}")
+    print("=========================================")
+
+    while True:
+        clientsocket, address = serverSocket.accept()
+
+        # Message of successfull connection!
+        print(f"Connection from {address} has been established.")
+        
+        username = clientsocket.recv(1024).decode()
+        
+        # Send acknowledgement of received username.
+        ack = "ACK: Username Received"
+        clientsocket.send(ack.encode())
+
+        password = clientsocket.recv(1024).decode()
+
+        # Check password and send acknowledgement
+        ack = checkPassword(username, password)
+        clientsocket.send(ack.encode())
+        
+        # Close socket and listen to other sockets
+        clientsocket.close()
+        
+
+
+
+if __name__ == "__main__":
+    main()
